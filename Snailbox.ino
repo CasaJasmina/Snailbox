e1172674f85e7bc0f111e23bb0d269e29610ec0d
giacomoleonzi <giac.leonzi@gmail.com>
2016-09-26 22:23:45 +0200

    todo: LDR sensor, and IFTTT POST call

---
+++ Snailbox.ino
@@ -0,0 +1,130 @@
+/****************************************************************************
+  SNAILBOX
+
+  giacomoleonzi@gmail.com
+
+  this sketch is for a proejct called SNAILBOX, snailbox.cj@gmail.com
+
+  major features:
+
+  - connect to WPA SSID
+
+  - manage colors, intensity and animation of neopixel(ws2812b)
+
+  - read data coming from an 3x accelerometer (MMA7660) the grove one
+
+
+******************************************************************************/
+#include "axis.h"
+#include "strip.h"
+#include "wifi.h"
+
+
+WiFiClient client;
+
+int delayval = 500; // delay for half a second
+
+/*
+
+                    SETUP
+
+*/
+
+void setup()
+{
+  // IF YOU ARE USING AN AVR_ATtiny85 YOU NEED THIS
+#if defined (__AVR_ATtiny85__)
+  if (F_CPU == 16000000) clock_prescale_set(clock_div_1);
+#endif
+
+
+  Serial.begin(9600);
+
+  //**********************************************************
+  //*************init  led strip******************************
+  //**********************************************************
+  stripBegin();
+
+  //**********************************************************
+  //*************starting accellerometer setup****************
+  //**********************************************************
+  accelBegin();
+
+  //**********************************************************
+  //************STARTING CONNECTION***************************
+  //**********************************************************
+  startConnection();
+}
+
+/*
+
+                     LOOP
+
+
+*/
+
+int f = 0;
+int delta = 10;
+int r = 0;
+uint16_t s = 0;
+
+void loop()
+{
+  int mood = getPos();
+
+  switch (mood) {
+
+
+    //**********************
+    //relax mood, warm light
+    case 0:
+      {
+        Serial.println("relax");
+        //send_event("relax");
+        f = 0;
+        r = rainbow(20, r);;
+        break;
+      }
+
+
+    //**********************
+    //focus mood, strong light
+    case 1:
+      {
+        Serial.println("focus");
+        //send_event("focus");
+        r = 0;
+        f = focus(f);
+        break;
+      }
+
+
+    //**********************
+    //Sprint  mood, rainbow light
+    case 2:
+      {
+        Serial.println("Sprint");
+        //send_event("sprint");
+        s = sprint(s);
+        break;
+      }
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ strip.h
@@ -0,0 +1,88 @@
+#include <Adafruit_NeoPixel.h>           //neopixel
+#ifdef __AVR__
+#include <avr/power.h>
+#endif
+
+#define LEDPIN            2  //pin to drive pixels
+#define NUMPIXELS         17  //try to guess? 
+
+
+
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUMPIXELS, LEDPIN, NEO_GRB + NEO_KHZ800);
+
+
+//**********************************************************
+//************RELAX MOOD************************************
+//**********************************************************
+uint32_t focus(int r) {
+  if (r < 255)r += 20;
+  r = constrain(r, 0, 255);
+  uint32_t c = strip.Color(r, (r * 4) / 5, r / 5);
+  for (int i = 0; i < strip.numPixels(); i++) {
+    strip.setPixelColor(i, c);
+  }
+  strip.show();
+  return r;
+}
+
+
+//**********************************************************
+//************FOUCS MOOD************************************
+//**********************************************************
+uint32_t sprint(int f) {
+  if (f < 255)f += 20;
+  f = constrain(f, 0, 255);
+  uint32_t c = strip.Color(f, f, f);
+  for (int i = 0; i < strip.numPixels(); i++) {
+    strip.setPixelColor(i, c);
+  }
+  strip.show();
+  return f;
+}
+
+
+//**********************************************************
+//************DO A WHEEL************************************
+//**********************************************************
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+uint32_t Wheel(byte WheelPos) {
+  WheelPos = 255 - WheelPos;
+  if (WheelPos < 85) {
+    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  }
+  if (WheelPos < 170) {
+    WheelPos -= 85;
+    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+  WheelPos -= 170;
+  return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+}
+
+
+//**********************************************************
+//************RAINBOW***************************************
+//**********************************************************
+uint16_t rainbow(uint8_t wait, uint16_t j) {
+  uint16_t i;
+  if (j > 255) j = 0;
+  for (i = 0; i < strip.numPixels(); i++) {
+    strip.setPixelColor(i, Wheel((i + j) & 255));
+  }
+  strip.show();
+  delay(wait);
+  return j + 1;
+}
+
+
+//**********************************************************
+//************stripBegin************************************
+//**********************************************************
+void stripBegin() {
+  strip.begin(); // This initializes the NeoPixel library.
+  strip.show(); // Initialize all pixels to 'off'
+  Wire.begin();
+  delay(1);
+}
+
+
